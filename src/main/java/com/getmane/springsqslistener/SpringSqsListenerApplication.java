c975a6f3a6d3eb627e65c1353ad7d6c09f4ad9c2
mrhul <mr.huliganer@gmail.com>
2023-05-01 18:08:59 +0300

    Initial configuration

---
+++ src/main/java/com/getmane/springsqslistener/SpringSqsListenerApplication.java
@@ -0,0 +1,13 @@
+package com.getmane.springsqslistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSqsListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSqsListenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/getmane/springsqslistener/SpringSqsListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getmane.springsqslistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSqsListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
